bbc16257f6e46cf578ffa965f338476d069bb2fb
hamriyanto <amriyanto50@yahoo.com>
2017-03-24 18:23:08 +0800

    postes04

--- src/postes_visual4/postes4.java
+++ src/postes_visual4/postes4.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package postes_visual04;
+package postes_visual4;
 
 
 public class postes4 extends javax.swing.JFrame {
